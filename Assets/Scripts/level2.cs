9381efe87c31b2a401ca1a9ac636a6bcabf1d462
Le Roux Matthieu <amiralprostate2708@gmail.com>
2023-04-02 14:51:19 +0200

    Update level2.cs

--- Assets/Scripts/level2.cs
+++ Assets/Scripts/level2.cs
@@ -15,31 +15,67 @@ public class level2 : MonoBehaviour
         particle = GameObject.Find("Particle");
         //umbrella = GameObject.Find("Umbrella");
 
-        int neuron1 = 0, neuron2 = 0, neuron3 = 0;
+        int neuron1 = 0, neuron2 = 0, neuron3 = 0, neuron4 = 0;
         try
         {
             neuron1 = PlayerPrefs.GetInt("Neurone (1)_levelValue");
             neuron2 = PlayerPrefs.GetInt("Neurone (2)_levelValue");
             neuron3 = PlayerPrefs.GetInt("Neurone (3)_levelValue");
+            neuron4 = PlayerPrefs.GetInt("Neurone (4)_levelValue");
         }
         catch (System.Exception e)
         {
             Debug.Log(e);
         }
-
-        if ((neuron1 == 1 && neuron2 == 0 && neuron3 == 1) || (neuron1 == 1 && neuron2 == 0 && neuron3 == 0) || (neuron1 == 0 && neuron2 == 0 && neuron3 == 1))
-        {
+        
+        if(neuron1 == 0 && neuron2 == 0 && neuron3 == 0 && neuron4 == 1)
+        {   //colere
+            //pas assez fort
+            scenarioChoice = 4;
+        }
+        else if (neuron1 == 0 && neuron2 == 0 && neuron3 == 1 && neuron4 == 0)
+        {   //protege plante
+            //pas assez fort
             scenarioChoice = 1;
         }
-        else if (neuron1 == 0 && neuron2 == 1 && neuron3 == 1)
-        {
+        else if (neuron1 == 0 && neuron2 == 1 && neuron3 == 0 && neuron4 == 0)
+        {   //colere
+            //pas assez fort
+            scenarioChoice = 4;
+        }
+        else if (neuron1 == 1 && neuron2 == 0 && neuron3 == 0 && neuron4 == 0)
+        {   //sommeil
+            scenarioChoice = 3;
+        }
+        else if (neuron1 == 0 && neuron2 == 0 && neuron3 == 1 && neuron4 == 1)
+        {   //tabasse
+            //pas assez fort
+            scenarioChoice = 4;
+        }
+        else if (neuron1 == 0 && neuron2 == 1 && neuron3 == 0 && neuron4 == 1)
+        {   //tabasse
+            // assez fort
             scenarioChoice = 0;
         }
-        else
-        {
-            scenarioChoice = 2;
+        else if (neuron1 == 1 && neuron2 == 0 && neuron3 == 0 && neuron4 == 1)
+        {   //tabasse
+            //pas assez fort
+            scenarioChoice = 4;
         }
-
+        else if (neuron1 == 0 && neuron2 == 1 && neuron3 == 1 && neuron4 == 0)
+        {   //tabasse
+            scenarioChoice = 4;
+        }
+        else if (neuron1 == 1 && neuron2 == 1 && neuron3 == 0 && neuron4 == 0)
+        {   //tabasse
+            //pas assez fort
+            scenarioChoice = 4;
+        }
+        else
+        {   //tabasse
+            //pas assez fort
+            scenarioChoice = 4;
+        }
     }
 
     public int getScenarioChoice()
@@ -51,7 +87,7 @@ public class level2 : MonoBehaviour
     {
         switch (choice)
         {
-            /*AGGRESSIF*/
+            /*AGGRESSIF * 2*/
             case 0:
                 yurE.GetComponent<yurE_level2>().frapperPlante();
                 StartCoroutine(waitToLoadVictory());
@@ -70,6 +106,10 @@ public class level2 : MonoBehaviour
             case 3:
                 yurE.GetComponent<machineAnimation>().sommeil();
                 StartCoroutine(waitToGameOver());
+                break;
+            case 4:
+                yurE.GetComponent<machineAnimation>().frapperParticule();
+                StartCoroutine(waitToLoadVictory());
                 break;
             default:
                 break;

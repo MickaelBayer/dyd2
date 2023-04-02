d5b08ab797e2d21a27efd61941fb3d13a73fd622
Le Roux Matthieu <amiralprostate2708@gmail.com>
2023-04-02 10:48:43 +0200

    particule mobile

--- Assets/Scripts/particuleBehaviour.cs
+++ Assets/Scripts/particuleBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class particuleBehaviour : MonoBehaviour
 {
@@ -10,25 +11,58 @@ public class particuleBehaviour : MonoBehaviour
     float particuleSpeed;
     [SerializeField]
     float distanceMiniArret;
+    [SerializeField]
+    float distanceMiniChangementDirection;
     bool isFrapped;
 
+    /*[SerializeField]
+    GameObject destinationImpact;*/
     [SerializeField]
-    GameObject destinationImpact;
+    GameObject[] destinationsBeforeImpact;
     [SerializeField]
     float speedChuteParticle;
     [SerializeField]
     bool isFalling;
 
+    [SerializeField]
+    GameObject yurE;
+
+    private int compteurDestination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        compteurDestination = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dir = destinationsBeforeImpact[compteurDestination].transform.position - transform.position;
+        float angle = -90 + Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (isFalling)
+        {
+            float distanceBeforeDestination = Vector2.Distance(this.transform.position, destinationsBeforeImpact[compteurDestination].transform.position);
+            if (distanceBeforeDestination >= this.distanceMiniChangementDirection)
+            {
+                Debug.Log("La particule de merde est à " + distanceBeforeDestination + " distance, elle change de direction en arrivant à " + this.distanceMiniChangementDirection);
+                this.transform.position = Vector2.Lerp(this.transform.position, destinationsBeforeImpact[compteurDestination].transform.position, Time.deltaTime * this.speedChuteParticle);
+            }
+            else
+            {
+                if (compteurDestination < destinationsBeforeImpact.Length - 1)
+                {
+                    compteurDestination++;
+                }
+                if(compteurDestination == destinationsBeforeImpact.Length - 1)
+                {
+                    yurE.GetComponent<machineAnimation>().anim_running = true;
+                }
+            }
+
+        }
     }
 
     private void FixedUpdate()
@@ -41,11 +75,6 @@ public class particuleBehaviour : MonoBehaviour
             {
                 this.transform.position = Vector2.Lerp(this.transform.position, destination.transform.position, Time.deltaTime * this.particuleSpeed);
             }
-        }
-        if (isFalling)
-        {
-            this.transform.position = Vector2.Lerp(this.transform.position, destinationImpact.transform.position, Time.deltaTime * this.speedChuteParticle);
-
         }
     }
 
